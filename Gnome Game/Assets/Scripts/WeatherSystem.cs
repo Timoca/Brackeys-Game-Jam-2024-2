9a37d695d199e436076242feea5d8b63983f64e6
Tim Geens <timgeens@msn.com>
2024-09-13 14:23:24 +0200

    The gameworld gets darker over time now.

--- Gnome Game/Assets/Scripts/WeatherSystem.cs
+++ Gnome Game/Assets/Scripts/WeatherSystem.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeatherSystem : MonoBehaviour
 {
+    [SerializeField] float _minBackgroundDimmerAlpha = 0.5f;
+    [SerializeField] float _maxBackgroundDimmerAlpha = 1f;
     [SerializeField] ParticleSystem _windParticlesBack;
     [SerializeField] ParticleSystem _windParticlesFront;
+    [SerializeField] RawImage _backgroundDimmer;
+    [SerializeField] Light _pointLight;
     private GameTimer _gameTimer;
     private CameraMovement _cameraMovement;
     private int _faseLength;
+    private float _elapsedTime = 0f;
     void Start()
     {
         _gameTimer = FindAnyObjectByType<GameTimer>();
@@ -15,6 +21,36 @@ public class WeatherSystem : MonoBehaviour
 
         _faseLength = _gameTimer.lengthOfGame / 3;
         StartCoroutine(StartStormyWeather());
+    }
+
+    void Update()
+    {
+        IncreaseDarkness();
+
+    }
+
+    private void IncreaseDarkness()
+    {
+        // Increase the elapsed time
+        _elapsedTime += Time.deltaTime;
+
+        // Calculate the interpolation factor (should be between 0 and 1)
+        float lerpFactor = _elapsedTime / _gameTimer.lengthOfGame;
+
+        // Make sure the interpolation factor is not greater than 1
+        lerpFactor = Mathf.Clamp01(lerpFactor);
+
+        // Get the current color
+        Color color = _backgroundDimmer.color;
+
+        // Adjust the alpha with the correct lerp factor
+        color.a = Mathf.Lerp(_minBackgroundDimmerAlpha, _maxBackgroundDimmerAlpha, lerpFactor);
+
+        // Adjust the intensity of the light
+        _pointLight.intensity = Mathf.Lerp(5000f, 2000f, lerpFactor);
+
+        // Set the modified color back
+        _backgroundDimmer.color = color;
     }
 
     private IEnumerator StartStormyWeather()
